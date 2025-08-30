743e47fb112746561edfeadf1b059c64ade3e6e1
onequick74 <onequick74@gmail.com>
2025-08-30 10:33:40 +0530

    Update __init__.py

--- devgagan/core/mongo/__init__.py
+++ devgagan/core/mongo/__init__.py
@@ -1,2 +1,13 @@
-# STAR JAAT
-# ---------------------------------------------------
+# devgagan/core/mongo/__init__.py
+
+from motor.motor_asyncio import AsyncIOMotorClient
+from config import MONGO_DB
+
+# MongoDB connection
+mongo_client = AsyncIOMotorClient(MONGO_DB)
+db = mongo_client["telegram_bot"]
+
+# Collections
+users_col = db["users"]
+plans_col = db["plans"]
+tokens_col = db["tokens"]
