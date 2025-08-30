23f04aa07950047d1a53c6354ae270277b9ecaae
onequick74 <onequick74@gmail.com>
2025-08-30 11:09:11 +0530

    Update users_db.py

--- devgagan/core/mongo/users_db.py
+++ devgagan/core/mongo/users_db.py
@@ -2,17 +2,21 @@
 
 from . import users_col
 
-async def add_user(user_id: int, username: str = None):
+# Add user (if not exists)
+async def add_user(user_id: int, username: str = None) -> bool:
     user = await users_col.find_one({"user_id": user_id})
     if not user:
         await users_col.insert_one({
             "user_id": user_id,
             "username": username,
         })
-    return True
+        return True   # New user added
+    return False      # Already exists
 
+# Get single user
 async def get_user(user_id: int):
     return await users_col.find_one({"user_id": user_id})
 
+# Get all users (returns list of dicts)
 async def get_users():
-    return users_col.find({})
+    return [doc async for doc in users_col.find({})]
