4680de334cf2052454750f64fd504d1c7e929e7f
onequick74 <onequick74@gmail.com>
2025-08-30 16:58:01 +0530

    Update __init__.py

--- jaat/core/mongo/__init__.py
+++ jaat/core/mongo/__init__.py
@@ -1,1 +1,8 @@
+from config import MONGO_DB
+from motor.motor_asyncio import AsyncIOMotorClient
 
+# Initialize Mongo connection
+mongo_client = AsyncIOMotorClient(MONGO_DB)
+
+# Default database for bot
+db = mongo_client.bot_db
