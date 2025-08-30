6d69e1ad521c67d4cb7fff6f71bdbc28da6b5025
onequick74 <onequick74@gmail.com>
2025-08-30 17:01:11 +0530

    Update __init__.py

--- jaat/modules/__init__.py
+++ jaat/modules/__init__.py
@@ -1,1 +1,15 @@
+import glob
+import os
 
+# Collect all modules dynamically
+def __list_all_modules():
+    modules = []
+    files = glob.glob(os.path.join(os.path.dirname(__file__), "*.py"))
+    for f in files:
+        name = os.path.basename(f)[:-3]
+        if name == "__init__" or name.startswith("_"):
+            continue
+        modules.append(name)
+    return modules
+
+ALL_MODULES = __list_all_modules()
