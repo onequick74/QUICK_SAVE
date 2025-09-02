c27bc5e0d070736bf98041cb112e9694f2421372
onequick74 <onequick74@gmail.com>
2025-09-02 19:09:28 +0530

    Update id.py

--- jaat/modules/id.py
+++ jaat/modules/id.py
@@ -1,11 +1,1 @@
-from pyrogram import filters
-from jaat import app
-
-@app.on_message(filters.command("id"))
-async def get_id(_, message):
-    if message.chat.type in ["group", "supergroup", "channel"]:
-        await message.reply_text(f"Chat ID: `{message.chat.id}`")
-    elif message.reply_to_message:
-        await message.reply_text(f"Replied User ID: `{message.reply_to_message.from_user.id}`")
-    else:
-        await message.reply_text(f"Your User ID: `{message.from_user.id}`")
+#star jaat
