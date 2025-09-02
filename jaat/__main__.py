b416a32f99348d7f657c751e42d90ff0fd5d0b66
onequick74 <onequick74@gmail.com>
2025-09-02 19:02:44 +0530

    Update __main__.py

--- jaat/__main__.py
+++ jaat/__main__.py
@@ -1,3 +1,6 @@
+# STAR JAAT
+# ---------------------------------------------------
+
 import asyncio
 import importlib
 import gc
@@ -6,13 +9,16 @@ from jaat.modules import ALL_MODULES
 from jaat.core.mongo.plans_db import check_and_remove_expired_users
 from aiojobs import create_scheduler
 
+# ----------------------------Bot-Start---------------------------- #
+
 loop = asyncio.get_event_loop()
 
+# Function to schedule expiry checks
 async def schedule_expiry_check():
     scheduler = await create_scheduler()
     while True:
         await scheduler.spawn(check_and_remove_expired_users())
-        await asyncio.sleep(60)
+        await asyncio.sleep(60)  # Check every hour
         gc.collect()
 
 async def devggn_boot():
@@ -23,6 +29,11 @@ async def devggn_boot():
 📂 Bot Deployed successfully ...
 📝 Description: A Pyrogram bot for downloading files from Telegram channels or groups 
                 and uploading them back to Telegram.
+👨‍💻 Author: star jaat
+🗓️ Created: 2025-01-11
+🔄 Last Modified: 2025-01-11
+🛠️ Version: 2.0.5
+📜 License: MIT License
 ---------------------------------------------------
 """)
 
@@ -31,5 +42,8 @@ async def devggn_boot():
     await idle()
     print("Bot stopped...")
 
+
 if __name__ == "__main__":
     loop.run_until_complete(devggn_boot())
+
+# ------------------------------------------------------------------ #
